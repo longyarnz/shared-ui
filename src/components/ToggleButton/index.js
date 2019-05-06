79e068476e02961648d06eff268f3f1327b921f8
Ayodele Olalekan <longyarnz@gmail.com>
2019-05-06 10:24:07 +0100

    Add doc to toggle-button component

--- src/components/ToggleButton/index.js
+++ src/components/ToggleButton/index.js
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 import PrimaryButton from '../PrimaryButton';
 import './toggle-button.css';
 
+/**
+ * @name ToggleButton
+ * @extends `PrimaryButton`
+ * @description Renders an icon and a text into an `PrimaryButton`.
+ * @param {string} props.addClass Add extra class styles to overwrite default class.
+ * @param {number} props.width Width of the button: defaults to 40.
+ * @param {number} props.height Height of the button: defaults to 25.
+ * @param {string} props.color Color for the toggle: defaults to #3b73ff.
+ * @param {boolean} props.disabled Disables the toggle button.
+ * @param {function} props.onToggle onclick event function of the button: receives the state `Boolean` of the toggle as an argument.
+ * @returns {JSX.Element} A toggle button.
+ */
 export default function ToggleButton(props) {
     const [state, setState] = useState(props.on || false);
     const { width = 40, height = 25, color = '#3b73ff' } = props;
